679448042b945ed394c7fd671bb694a76a586baa
amoskarugo <karugoamos@gmail.com>
2026-04-02 11:11:33 +0300

    initial commit

---
+++ src/test/java/com/app/studentgradetracker/StudentgradetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.studentgradetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentgradetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
